e6952a969f7094db0961b325449d837c67212b6a
niepeng <niepengsmile@gmail.com>
2018-01-24 18:55:51 +0800

    添加自适应高度

--- js/setType.js
+++ js/setType.js
@@ -2,7 +2,9 @@ function setMain() {
     var main = document.querySelector(".main");
     var imgs = Array.from(main.querySelectorAll("img"));
     main.style.height = document.documentElement.clientHeight + "px";
-    imgs.forEach(ea => ea.style.heigt = document.documentElement.clientHeight + "px");
+    imgs.forEach(ea => {
+        ea.style.height = document.documentElement.clientHeight + "px";
+    });
     window.onresize = function () {
         main.style.height = document.documentElement.clientHeight + "px";
         imgs.forEach(eb => {
