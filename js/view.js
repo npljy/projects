ea76eab041ea5a06688c52beacc3fcfbeb540a6c
niepeng <niepengsmile@gmail.com>
2018-01-24 17:24:39 +0800

    github个人主页，翻页功能完成。

--- js/view.js
+++ js/view.js
@@ -1,13 +1,86 @@
 window.onload = function(){
-    var papers = document.querySelectorAll(".paper");
-    document.onclick = function(){
-        console.log(papers[0].style.transform) 
-        papers[0].style.transform = "rotateY(180deg)";
-        setTimeout(function(){
-            papers[0].querySelector(".leftPage").style.zIndex = "10";
-            papers[0].querySelector(".rightPage").style.zIndex = "20";
-        },250);
+    
+    var timer = null;
+    var timer2 = null;
+    var onoff = false;
+    var papers = Array.from(document.querySelectorAll(".paper"));
+    papers.forEach((e,i)=>{
+        e.style.zIndex = papers.length-1-i;
+        // if(i == papers.length-1)return;
+        e.onclick = function(){
+            turnPage(e,i);
+        }
+    });
+   
+    function turnPage(e,i){
+        if(!onoff){
+            onoff = true;
+            if(!e.dataset.click || e.dataset.click == "no"){
+                var numY = 0;
+                var numZ = 0;
+                papers.forEach(e =>e.style.left = "50%");
+                if(i==papers.length-1)papers.forEach(e =>e.style.left = "67%");
+                e.dataset.click = "yes";
+                timer = setInterval(function(){
+                    numY++;
+                    numZ = parseFloat(numZ.toFixed(2)) + 0.1;
+                    if(numY > 90){clearInterval(timer)};
+                    e.style.transform = "rotateY("+ (-numY) +"deg) skewY("+ (-numZ) +"deg)";
+                },10);
+                setTimeout(function(){
+                    numZ = parseFloat(numZ.toFixed(2)) - 0.1;
+                    timer2 = setInterval(function(){
+                        numZ = parseFloat(numZ.toFixed(2)) - 0.1;
+                        e.style.transform = "rotateY("+ (-numY) +"deg) skewY("+ (-numZ) +"deg)";
+                        numY++;
+                        if(numY > 180 ){clearInterval(timer2)};
+                    },10);
+                    setTimeout(function(){
+                        getComputedStyle(e).zIndex == i ? e.style.zIndex = papers.length-1-i : e.style.zIndex = i ;
+                        e.querySelector(".leftPage").classList.toggle("act");
+                    },10);
+                    setTimeout(function(){
+                        onoff = false;
+                    },500);
+                },1000)
+            }
+            else{
+                var numY = 180;
+                var numZ = 0;
+                e.dataset.click = "no";
+                papers.forEach(e =>e.style.left = "50%");
+                if(i==0)papers.forEach(e =>e.style.left = "30%");
+                timer = setInterval(function(){
+                    numY--;
+                    numZ = parseFloat(numZ.toFixed(2)) + 0.1;
+                    if(numY < 90){clearInterval(timer)};
+                    e.style.transform = "rotateY("+ (-numY) +"deg) skewY("+ (-numZ) +"deg)";
+                },10);
+                setTimeout(function(){
+                    numZ = parseFloat(numZ.toFixed(2)) - 0.1;
+                    timer2 = setInterval(function(){
+                        numZ = parseFloat(numZ.toFixed(2)) - 0.1;
+                        e.style.transform = "rotateY("+ (-numY) +"deg) skewY("+ (-numZ) +"deg)";
+                        numY--;
+                        if(numY < 0 ){clearInterval(timer2)};
+                    },10);
+                    setTimeout(function(){
+                        getComputedStyle(e).zIndex == i ? e.style.zIndex = papers.length-i : e.style.zIndex = i ;
+                        e.querySelector(".leftPage").classList.toggle("act");
+                    },10);
+                    setTimeout(function(){
+                        onoff = false;
+                    },500);
+                },1000)
+
+            }
+
+        }
     }
+
+
+
+
 
 
 }
