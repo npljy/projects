2be8599814b9c2686858b8729b60fafd462a034d
niepeng <niepengsmile@gmail.com>
2018-01-25 10:13:58 +0800

    修改导航自适应

--- js/setType.js
+++ js/setType.js
@@ -1,5 +1,5 @@
 function setMain() {
-    var main = document.querySelector(".main");
+    var main = document.querySelector(".content");
     var nav = document.querySelector(".nav");
     var imgs = Array.from(main.querySelectorAll("img"));
     var rightPage = Array.from(main.querySelectorAll(".rightPage"));
