10128d9f529bccb918f5cec24d7e0d2b7c717bc4
NiePeng <niepengsmile@gmail.com>
2018-02-27 11:08:22 +0800

    react云盘:完成双击文件夹进入下级目录功能

---
+++ reactYunPan/src/yun/comp/cont.js
@@ -0,0 +1,61 @@
+import React ,{Component} from "react"
+
+class Cont extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            val:this.props.title
+        }
+    }
+    cc = (oid)=>{
+        let {chgChked} = this.props;
+        chgChked(oid);
+    }
+    rn = ()=>{
+        console.log(111)
+    }
+    cv = (ev)=>{
+        this.setState({
+            val : ev.target.value
+        })
+    }
+    kd = (ev)=>{
+        if(ev.keyCode === 13){
+            let {oid,changeTitle} = this.props;
+            changeTitle(oid,ev.target.value);
+        }
+    }
+    cid = (num)=>{
+        let {changeid} = this.props;
+        changeid(num);
+    }
+    render(){
+        let {val} = this.state;
+        let {oid,pid,title,checked,rename} = this.props;
+        return (
+            <div className="file-item" oid={oid} pid={pid} >
+                <img 
+                    src={require("../img/folder-b.png")} 
+                    alt={require("../img/folder-b.png")}
+                    oid={oid} 
+                    onDoubleClick = {this.cid.bind(this,oid)}
+                />
+                <span className="folder-name"
+                    style={{display:rename?'none':'block'}}
+                    onDoubleClick = {this.rn}
+                >{title}</span>
+                <input type="text" className="editor"
+                    style={{display:rename?'block':'none'}}
+                    value = {val}
+                    onChange = {this.cv}
+                    onKeyDown = {this.kd}
+                />
+                <i className={checked ? 'checked' : ''}  oid={oid} pid={pid} 
+                    onClick = {this.cc.bind(this,oid)}
+                ></i>
+            </div>
+        )
+    }
+}
+
+export default Cont;
